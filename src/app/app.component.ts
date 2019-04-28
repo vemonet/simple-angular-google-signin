b01423ed24c1871099a508371e47c06616078a3d
vemonet <vincent.emonet@gmail.com>
2019-04-28 20:01:27 +0200

    try to implement google signin solution from https://stackoverflow.com/questions/38846232/how-to-implement-signin-with-google-in-angular-2-using-typescript/39770500 WORKING in console.log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
+declare const gapi: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'simple-google-signin';
+
+  private clientId: string = '964247135130-b586l1uobdvdom9slnavs75nrn66jfql.apps.googleusercontent.com';
+  
+  private scope = [
+    'profile',
+    'email',
+    'https://www.googleapis.com/auth/plus.me',
+    'https://www.googleapis.com/auth/contacts.readonly',
+    'https://www.googleapis.com/auth/admin.directory.user.readonly'
+  ].join(' ');
+
+  public auth2: any;
+  public googleInit() {
+    let that = this;
+    gapi.load('auth2', function () {
+      that.auth2 = gapi.auth2.init({
+        client_id: that.clientId,
+        cookiepolicy: 'single_host_origin',
+        scope: that.scope
+      });
+      that.attachSignin(that.element.nativeElement.firstChild);
+    });
+  }
+  public attachSignin(element) {
+    let that = this;
+    this.auth2.attachClickHandler(element, {},
+      function (googleUser) {
+
+        let profile = googleUser.getBasicProfile();
+        console.log('Token || ' + googleUser.getAuthResponse().id_token);
+        console.log('ID: ' + profile.getId());
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+
+      }, function (error) {
+        console.log(JSON.stringify(error, undefined, 2));
+      });
+  }
+
+  constructor(private element: ElementRef) {
+    console.log('ElementRef: ', this.element);
+  }
+
+  ngAfterViewInit() {
+    this.googleInit();
+  }
+
 }
